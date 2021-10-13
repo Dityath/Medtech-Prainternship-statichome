419066da96a6fd90296fc209140d599eb5129dff
dityath <dityaathallah107@gmail.com>
2021-10-13 23:34:01 +0700

    new-update

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,7 +1,32 @@
+import Iframe from "react-iframe";
+import { Image } from "@chakra-ui/react";
+
 const Footer = () => {
   return (
-    <footer className="h-96 w-full bg-gray-800 font-inter flex items-center">
-      <div></div>
+    <footer className="h-96 w-full bg-gray-800 font-inter flex items-center justify-between px-24">
+      <div className="flex gap-8">
+        <div>
+          <Image src="/images/footer/logo-small.png" />
+        </div>
+        <div className="w-2/4 text-white">
+          <h3 className="text-2xl font-bold mb-1">MechTech Indonesia</h3>
+          <p className="text-justify">
+            MechTech Indonesia adalah sebuah startup pelatihan software teknik
+            bagi mahasiswa teknik untuk mempersiapkan diri menghadapi dunia
+            kerja di era revolusi industri 4.0 dengan metode pelatihan khusus
+            berbentuk “Project Based Learning”
+          </p>
+        </div>
+      </div>
+      <div className="w-3/4 overflow-hidden rounded-2xl">
+        <Iframe
+          url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253292.42716058585!2d112.57259700215688!3d-7.27559793655625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fbf8381ac47f%3A0x3027a76e352be40!2sSurabaya%2C%20Surabaya%20City%2C%20East%20Java!5e0!3m2!1sen!2sid!4v1634141740211!5m2!1sen!2sid"
+          className="w-full"
+          height="250"
+          styles={{ border: 0 }}
+          loading="lazy"
+        />
+      </div>
     </footer>
   );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,9 +6,31 @@ import HomeFourth from "../components/home/HomeFourth";
 import HomeFifth from "../components/home/HomeFifth";
 import Footer from "../components/footer";
 
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
       <Navbar />
       <HomeFirst />
       <HomeSecond />
